8f8d017cd06e2cc292d4df8a623b259c44c0200b
Anowar Hossain <157941809+Anowar-Git24@users.noreply.github.com>
2024-09-20 23:00:31 -0400

    Add files via upload
    Initial Commit

---
+++ Selenium_Maven_Jenkins_project/src/main/java/Selenium_Maven_Jenkins_project/Selenium_Maven_Jenkins_project/App.java
@@ -0,0 +1,10 @@
+package Selenium_Maven_Jenkins_project.Selenium_Maven_Jenkins_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
